62d01a238114022295b4188a2128891407f57690
YAMA <145954469+VeroUfomia1398@users.noreply.github.com>
2024-10-31 11:51:16 +0900

    自動制御

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
+input.onPinPressed(TouchPin.P0, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+    for (let index = 0; index < 4; index++) {
+        GoStraight()
+        CornerTurn()
+    }
+})
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 180)
 })
+function GoStraight () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 90)
+    basic.pause(100)
+}
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
@@ -10,6 +23,11 @@ input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 0)
 })
+function CornerTurn () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
+    basic.pause(100)
+}
 basic.forever(function () {
 	
 })
