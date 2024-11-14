6e5e23664f61e7219e03731f0f458280a771363b
iaaaoo012 <166587202+iaaaoo012@users.noreply.github.com>
2024-11-14 11:46:32 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     Straight()
-    basic.pause(6000)
     Turn()
 })
 function Turn () {
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 180)
+    basic.pause(500)
 }
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P1, 90)
@@ -14,6 +14,7 @@ input.onButtonPressed(Button.AB, function () {
 function Straight () {
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 150)
+    basic.pause(6000)
 }
 basic.pause(60000)
 pins.servoWritePin(AnalogPin.P1, 90)
