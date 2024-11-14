c454b9b400d8b546937460fefba5a6cfac6fb631
YAMA <145954469+VeroUfomia1398@users.noreply.github.com>
2024-11-14 11:26:43 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
         Straight()
+        basic.pause(2000)
         Turn()
-        basic.pause(5000)
+        basic.pause(1000)
     }
 })
 function Turn () {
     pins.servoWritePin(AnalogPin.P1, 180)
-    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
 }
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
 })
 function Straight () {
-    pins.servoWritePin(AnalogPin.P1, 180)
-    pins.servoWritePin(AnalogPin.P2, 0)
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 180)
 }
+basic.pause(60000)
+pins.servoWritePin(AnalogPin.P1, 90)
+pins.servoWritePin(AnalogPin.P2, 90)
